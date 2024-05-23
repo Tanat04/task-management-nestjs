6f2dbcc36333f19a8510178cd7059ff38a48e31b
Tanat Arora <tanat.navin@gmail.com>
2024-05-23 20:59:44 +0700

    Validation and Error Handlings

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,13 @@
+import { IsEnum, IsNotEmpty, IsOptional, IsString, isString } from "class-validator";
 import { TaskStatus } from "../tasks.model";
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
+
+  @IsNotEmpty()
+  @IsOptional()
+  @IsString()
   search?: string;
 }
